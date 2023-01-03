2c7139525effe605ff5e682c797fbd91afe7c314
John Constant <johnconstantis@gmail.com>
2023-01-03 17:23:24 +0000

    Add Product and Category admin classes to admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
