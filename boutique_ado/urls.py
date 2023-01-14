4d9246f80214e2abcd28523c0b538facfae0f994
John Constant <johnconstantis@gmail.com>
2023-01-14 18:02:52 +0000

    Add profiles app with basic view, template and urls

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
     path('', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
