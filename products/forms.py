dceb5ffe1b229e5cc4b2d30559a2240c0011ba2e
John Constant <johnconstantis@gmail.com>
2023-01-17 19:23:12 +0000

    Begin fixing the image upload field

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from .widgets import CustomClearableFileInput
+
 from .models import Product, Category
 
 
@@ -7,6 +9,8 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
